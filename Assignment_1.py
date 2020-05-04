90b4a04a1124ccb5d070d1ce9a1baca37eddc291
sumedh15 <64272273+sumedh15@users.noreply.github.com>
2020-05-04 10:48:50 -0500

    Update Assignment_1.py

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,19 +1,16 @@
 ## Task 1.2 - Numbers divisible by 7 and not by 5
-
 for i in range(2000,3201):
     if i % 7 == 0 and i % 5 != 0:
         new.append(i)
 print(new)
 
 ## Task 1.3 - User input and reverse
-
 First_Name = input("Enter first Name: ")
 Last_Name = input("Enter last Name: ")
 
 print(First_Name[::-1] + " " + Last_Name[::-1])
 
 ## Task 1.4 - Sphere volume
-
 D = 12
 pi = 3.142
 Sphere_formula = 4/3*pi*((D/2)**3)
@@ -21,7 +18,6 @@ Sphere_formula = 4/3*pi*((D/2)**3)
 print("the volume of the sphere is ",Sphere_formula)
 
 ## Task 2.1 - Sequence list for consecutive numbers
-
 List = []
 Lower = int(input("Enter the lower range: "))
 Upper = int(input("Enter the upper range: "))
@@ -31,7 +27,6 @@ for i in range (Lower,Upper+1):
 print(List)
 
 ## Task 2.2 - Pattern using nested loop
-
 rows = int(input("enter the number of rows: "))
 
 for i in range(0,rows):
@@ -45,7 +40,6 @@ for i in range(rows,0,-1):
     print("\r")
 
 ## Task 2.3 - Reverse User input
-
 User_input = input("enter the input: ")
 
 print("The reverse order of the user input is ", User_input[::-1])
